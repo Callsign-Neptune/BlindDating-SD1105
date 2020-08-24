fc15811a6a72fd75499bbb7d079004404caf52a2
Callsign-Neptune <66689267+Callsign-Neptune@users.noreply.github.com>
2020-08-24 04:55:28 -0500

    Add files via upload

---
+++ Models/DatingProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlindDating.Models
+{
+    public partial class DatingProfile
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Bio { get; set; }
+        public string UserAccountId { get; set; }
+    }
+}
